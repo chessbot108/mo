f1ac4eaf2d71d15012557e441da07a558579c22e
chessbot108 <66888352+chessbot108@users.noreply.github.com>
2022-11-06 21:14:30 -0600

    lis coded

---
+++ prog/control.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cin.tie(0) -> sync_with_stdio(0);
+	int n, D;
+	cin >> n >> D;
+	for(int i = 0; i<n; i++){
+		int x, y;
+		cin >> x >> y;
+	}
+	for(int i = 0; i<n; i++){
+		cout << i << " ";
+	} cout << "\n";
+}
--- prog/lis.cpp
+++ prog/lis.cpp
@@ -25,7 +25,7 @@
 
 using namespace std;
 
-vector<pair<int, int>> lis(vector<pair<int, int>> arr){
+vector<int> lis(vector<pair<int, int>> arr){
 	set<pair<pair<int, int>, int>> s;
 	for(int i = 0; i<sz(arr); i++){
 		auto [y, j] = arr[i];
@@ -36,10 +36,12 @@ vector<pair<int, int>> lis(vector<pair<int, int>> arr){
 			s.erase(it);
 		}
 	}
-	vector<pair<int, int>> ans;
-	for(auto [y, i, j] : s){
-		ans.pb(
+	vector<int> ans;
+	for(auto [p, j] : s){
+		auto [y, i] = p;
+		ans.push_back(j);
 	}
+	return ans;
 }
 
 signed main(){
@@ -47,33 +49,84 @@ signed main(){
 	int n, D;
 	cin >> n >> D;
 	vector<pair<pair<int, int>, int>> vec;
+	vector<int> vis(n+1, 0);
+	vector<pair<int, int>> points;
 	for(int i = 0; i<n; i++){
 		int x, y;
 		cin >> x >> y;
 		vec.push_back(make_pair(make_pair(x, y), i));
+		points.push_back({x, y});
 	}
 
 
 	vector<int> p;
 	while(sz(vec)){
 		vector<pair<int, int>> up, down;
-		sort(all(vec), [&](pair<int, pair<int, int>, int> a, pair<pair<int, int>, int> b){
+		sort(all(vec), [&](pair<pair<int, int>, int> a, pair<pair<int, int>, int> b){
 				return a < b;	
 				});
+		//cerr << "up\n";
 		for(int i = 0; i<sz(vec); i++){
 			up.push_back(make_pair(vec[i].first.second, vec[i].second));
+			//cerr << vec[i].second << " " << vec[i].first.first << " " << vec[i].first.second << "\n";
+
 		}
-		sort(all(vec), [&](pair<int, pair<int, int>, int> a, pair<pair<int, int>, int> b){
+		sort(all(vec), [&](pair<pair<int, int>, int> a, pair<pair<int, int>, int> b){
 				if(a.first.first != b.first.first){
 					return a.first.first > b.first.first;
 				}	
 				return make_pair(a.first.second, a.second) < make_pair(b.first.second, b.second);
 				});
+		//cerr << "down\n";
 		for(int i = 0; i<sz(vec); i++){
 			down.push_back(make_pair(vec[i].first.second, vec[i].second));
+			//cerr << vec[i].second << " " << vec[i].first.first << " " << vec[i].first.second << "\n";
 		}
-
+		//reverse(all(down));
+		auto uplis = lis(up), downlis = lis(down);
+		vector<pair<pair<int, int>, int>> newvec;
+		if(sz(uplis) > sz(downlis)){
+			//cerr << "uplis\n";
+			for(int x : uplis){
+				//cerr << x << " " ;
+				vis[x] = 1;
+				p.push_back(x);
+			}
+			//cerr << "\n";
+			for(int i = 0; i<sz(vec); i++){
+				if(!vis[vec[i].second]){
+					newvec.push_back(vec[i]);
+				}
+			}
+			for(int x : uplis){
+				vis[x] = 0;
+			}
+			
+		}else{
+			//cerr << "downlis\n";
+			for(int x : downlis){
+				//cerr << x << " ";
+				vis[x] = 1;
+				p.push_back(x);
+			}
+			//cerr << "\n";
+			for(int i = 0; i<sz(vec); i++){
+				if(!vis[vec[i].second]){
+					newvec.push_back(vec[i]);
+				}
+			}
+			for(int x : downlis){
+				vis[x] = 0;
+			}
+		}	
+		vec = newvec;
 	}
+	for(int x : p){
+		cout << x << " " ;
+	} cout << "\n";
+	//for(int i = 0; i<sz(p); i++){
+		//cerr << points[p[i]].first << " " << points[p[i]].second << "\n";
+	//}
 	return 0;
 }
 
